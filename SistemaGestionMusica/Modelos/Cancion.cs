67ea983890869493ef3826b4d9e4a662bd8f0a2f
Fernando Alfaro <falfarocruz27@gmail.com>
2025-11-12 11:18:26 -0600

    Modelos

---
+++ SistemaGestionMusica/Modelos/Cancion.cs
@@ -0,0 +1,26 @@
+﻿
+namespace SsitemaMusica.Modelos;
+// Clase para canciones
+public class Cancion
+{
+    // Atributos de la clase Cancion
+    public string Nombre { get; set; }
+    public string Artista { get; set; }
+    public int Duracion { get; set; } // En segundos   
+
+    // Constructor
+    public Cancion(string nombre, string artista, int duracion)
+    {
+        Nombre = nombre;
+        Artista = artista;
+        Duracion = duracion;
+    }
+
+    // Métodos
+    public override string ToString()
+    {
+        int minutos = Duracion / 60;
+        int segundos = Duracion % 60;
+        return $"{Nombre} - {Artista} ({minutos}:{segundos})";
+    }
+}
--- SistemaGestionMusica/Modelos/Usuario.cs
+++ SistemaGestionMusica/Modelos/Usuario.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SistemaGestionMusica.Modelos
+﻿namespace SsitemaMusica.Modelos;
+// Clase para usuarios
+public class Usuario
 {
-    internal class Usuario
+    // Atributos de la clase Usuario
+    public string Nombre { get; set; }
+    public Dictionary<string, List<Cancion>> ListasReproduccion { get; set; } // Diccionario para listas de reproducción, clave: nombre de la lista, valor: lista de canciones
+
+    //Constructor de la clase Usuario
+    public Usuario(string nombre)
     {
+        Nombre = nombre;
+        ListasReproduccion = new Dictionary<string, List<Cancion>>();
+    }
+
+    // Métodos
+    public void CrearListaReproduccion(string nombre)
+    {
+        if (!ListasReproduccion.ContainsKey(nombre))
+        {
+            ListasReproduccion[nombre] = new List<Cancion>();
+            Console.WriteLine($"La Lista de Reproducción: {nombre} ha sido creada con éxito");
+        }
+        else
+        {
+            Console.WriteLine($"La Lista de Reproducción: {nombre} ya existe");
+        }
+    }
+
+    public void AgregarCancionALista(string nombreLista, Cancion cancion)
+    {
+        if (ListasReproduccion.ContainsKey(nombreLista))
+        {
+            ListasReproduccion[nombreLista].Add(cancion);
+            Console.WriteLine($"La canción: {cancion.Nombre} ha sido agregada a la lista: {nombreLista}");
+        }
+        else
+        {
+            Console.WriteLine($"La Lista de Reproducción: {nombreLista} no existe");
+        }
+    }
+
+    public void MostrarListasReproduccion()
+    {
+        foreach (KeyValuePair<string, List<Cancion>> lista in ListasReproduccion)
+        {
+            Console.WriteLine($"Lista: {lista.Key}");
+            foreach (Cancion cancion in lista.Value)
+            {
+                Console.WriteLine(cancion.ToString());
+            }
+        }
     }
 }
